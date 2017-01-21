891c92d6331278ac3e2d681f54c9500128db40b2
jasaaved <jasaaved@uci.edu>
2017-01-21 11:34:40 -0800

    Elephant moves and shoot with a controller

--- wavesofwaves/Assets/Scripts/JoystickController.cs
+++ wavesofwaves/Assets/Scripts/JoystickController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class JoystickController : MonoBehaviour
 {
     public float speed = 6f;            // The speed that the player will move at.
@@ -9,12 +8,14 @@ public class JoystickController : MonoBehaviour
     Vector3 movement;                   // The vector to store the direction of the player's movement.
     Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
     private float attackTimer;
+    public float xVelAdj;
+    public float yVelAdj;
     private float xFire;
     private float yFire;
     public GameObject Airblast;
 
     void Awake()
-    { 
+    {
         // Set up references.
         playerRigidbody = GetComponent<Rigidbody>();
     }
@@ -27,16 +28,16 @@ public class JoystickController : MonoBehaviour
 
     void FixedUpdate()
     {
-        float h = Input.GetAxis("xMove");
-        float v = Input.GetAxis("yMove");
+        xVelAdj = Input.GetAxis("xMove");
+        yVelAdj = Input.GetAxis("yMove");
         xFire = Input.GetAxis("xShoot");
         yFire = Input.GetAxis("yShoot");
-        Move(h, v);
+        Move(xVelAdj, yVelAdj, xFire, yFire);
     }
 
     private void Update()
     {
-        if((Mathf.Abs(xFire) > 0.2 || Mathf.Abs(yFire) > 0.2) && attackTimer <= 0)
+        if ((Mathf.Abs(xFire) > 0.2 || Mathf.Abs(yFire) > 0.2) && attackTimer <= 0)
         {
             AirBlast();
             attackTimer = 1f;
@@ -44,8 +45,10 @@ public class JoystickController : MonoBehaviour
         attackTimer -= Time.deltaTime;
     }
 
-    void Move(float h, float v)
+    void Move(float h, float v, float xs, float ys)
     {
+        /*
+        //Does not work
         // Set the movement vector based on the axis input.
         movement.Set(h, 0f, v);
 
@@ -54,6 +57,10 @@ public class JoystickController : MonoBehaviour
 
         // Move the player to it's current position plus the movement.
         playerRigidbody.MovePosition(transform.position + movement);
+        */
+        playerRigidbody.velocity = new Vector3(3 * h, 0, 3 * v);
+        float heading = Mathf.Atan2(xs, ys);
+        transform.rotation = Quaternion.EulerAngles(0, heading, 0);
     }
 
     void AirBlast()
