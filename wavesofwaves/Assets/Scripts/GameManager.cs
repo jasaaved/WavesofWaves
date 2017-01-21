c3265023c6ca0f6ba5aa89404f0c60ad4a7b6a98
Johnny Nguyen <jonthnngyn@gmail.com>
2017-01-21 10:15:54 -0800

    Stub out game manager

--- wavesofwaves/Assets/Scripts/GameManager.cs
+++ wavesofwaves/Assets/Scripts/GameManager.cs
@@ -1,16 +1,116 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static GameManager Instance { get { return _instance; } }
+
+    public enum StateType
+    {
+        DEFAULT,      //Fall-back state, should never happen
+        PLAYING,      //waiting for other player to finish his turn
+        SHOP,    //Once, on start of each player's turn
+        GAMEOVER,
+    };
+
+    // Game state
+    public StateType currentState;
+    public bool isGameOver;
+    public bool enemiesDisabled;
+    public bool musicDisabled;
+
+    // Level
+    public int score;
+    public int currentLevel;
+
+    // Components
+    public Transform player;
+
+    // UI
+    public GameObject gameoverMenu;
+    public GameObject HUDMenu;
+    public Text scoreText;
+    public Text hiScore;
+    public Text displayText;
+    public Canvas canvas;
+
+    // Audio 
+    public AudioSource sfxSource;
+    public AudioClip levelUpSound;
+    public AudioClip beep;
+
+
+    public void Start()
+    {
+        currentState = StateType.PLAYING;
+        Time.timeScale = 1;
+
+        isGameOver = false;
+    }
+
+    public void Update()
+    {
+        if (isGameOver)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        HUDMenu.SetActive(false);
+    }
+
+    public void PlayClip(AudioClip clip, float volume, bool isLooping)
+    {
+        sfxSource.clip = clip;
+        sfxSource.volume = volume;
+        sfxSource.loop = isLooping;
+        sfxSource.Play();
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public void CheckEnemies()
+    {
+
+    }
+ 
+    public void IncrementLevel()
+    {
+
+    }
+
+    public void DisplayText(string text)
+    {
+        Text tempTextBox = Instantiate(displayText, new Vector3(1, -13, 0), transform.rotation) as Text;
+        tempTextBox.transform.SetParent(canvas.transform, false);
+        tempTextBox.text = text;
+        tempTextBox.CrossFadeAlpha(0f, 2.5f, true);
+        Destroy(tempTextBox, 2.5f);
+    }
+
+    public void DisplayText(string text, Color color)
+    {
+        Text tempTextBox = Instantiate(displayText, new Vector3(1, -13, 0), transform.rotation) as Text;
+        tempTextBox.transform.SetParent(canvas.transform, false);
+        tempTextBox.text = text;
+        tempTextBox.color = color;
+        tempTextBox.CrossFadeAlpha(0f, 2.5f, true);
+        Destroy(tempTextBox, 2.5f);
+    }
 }
