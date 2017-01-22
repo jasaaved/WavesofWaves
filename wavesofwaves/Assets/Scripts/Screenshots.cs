58a12ac147c484948df5a6871d436bec6cf95842
Roger <roger@dalkefamily.com>
2017-01-22 15:45:35 -0800

    Added splash image and take screenshots

---
+++ wavesofwaves/Assets/Scripts/Screenshots.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Screenshots : MonoBehaviour
+{
+    static int capNum;
+	
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.P))
+        {
+            Application.CaptureScreenshot("Screenshot " + capNum.ToString() + ".png");
+        }
+	}
+}
