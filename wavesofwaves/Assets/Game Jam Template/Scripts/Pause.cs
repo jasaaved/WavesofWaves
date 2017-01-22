8db6d5aa7ee689c61c1359ffcb0375b71c9ab4c6
Roger <roger@dalkefamily.com>
2017-01-22 15:35:10 -0800

    Added controller input for menus and pausing

--- wavesofwaves/Assets/Game Jam Template/Scripts/Pause.cs
+++ wavesofwaves/Assets/Game Jam Template/Scripts/Pause.cs
@@ -21,13 +21,13 @@ public class Pause : MonoBehaviour {
 	void Update () {
 
 		//Check if the Cancel button in Input Manager is down this frame (default is Escape key) and that game is not paused, and that we're not in main menu
-		if (Input.GetButtonDown ("Cancel") && !isPaused && !startScript.inMainMenu) 
+		if (Input.GetButtonDown ("Pause") && !isPaused && !startScript.inMainMenu) 
 		{
 			//Call the DoPause function to pause the game
 			DoPause();
 		} 
 		//If the button is pressed and the game is paused and not in main menu
-		else if (Input.GetButtonDown ("Cancel") && isPaused && !startScript.inMainMenu) 
+		else if (Input.GetButtonDown ("Pause") && isPaused && !startScript.inMainMenu) 
 		{
 			//Call the UnPause function to unpause the game
 			UnPause ();
