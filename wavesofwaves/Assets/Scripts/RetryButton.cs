2b0edd93b325007dc6a2ae811dbdc13a3ac014ca
Johnny Nguyen <jonthnngyn@gmail.com>
2017-01-22 15:49:53 -0800

    Retry functionality

--- wavesofwaves/Assets/Scripts/RetryButton.cs
+++ wavesofwaves/Assets/Scripts/RetryButton.cs
@@ -5,6 +5,13 @@ public class RetryButton : MonoBehaviour {
 
     public void RetryGame()
     {
+        GameManager.scoreInt = 0;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void TitleScreen()
+    {
+        GameManager.scoreInt = 0;
+        SceneManager.LoadScene(0);
     }
 }
