04cff1e800f4164edfd5a7c29fe28782d5e59aaa
Alex Bagg <abagg@uci.edu>
2017-01-21 20:44:50 -0800

    Re-added lighting data

--- wavesofwaves/Assets/Scripts/PlayerController.cs
+++ wavesofwaves/Assets/Scripts/PlayerController.cs
@@ -49,7 +49,6 @@ public class PlayerController : MonoBehaviour
         airTimer = 0;
         waterTimer = 0;
         lightTimer = 0;
-        airCooldown = 1.25f;
     }
 
     void FixedUpdate()
